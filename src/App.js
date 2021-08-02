d173399790129c19a8cb4fbe657b6507e9ce7d39
akhil1610 <akhilb.1610@gmail.com>
2021-08-02 15:02:20 +0530

    nav bar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 import Main from './page/main/Main'
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Test from "./pages/Test";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
-   <Main/>
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/Profile' component={Profile} />
+        <Route path='/Test' component={Test} />
+      </Switch>
+    </Router>
+  </>
   );
 }
 
